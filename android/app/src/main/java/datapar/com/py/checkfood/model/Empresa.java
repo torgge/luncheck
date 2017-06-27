22444b76a48e128d1293bf06dcf90342761d7d48
Renato Pradebon <renato.pradebon@datapar.com.py>
2017-06-27 12:55:23 -0400

    Adicionando git ignore e pasta do android

---
+++ android/app/src/main/java/datapar/com/py/checkfood/model/Empresa.java
@@ -0,0 +1,24 @@
+package datapar.com.py.checkfood.model;
+
+public class Empresa {
+    private String nomeEmpresa;
+
+    private String responsavel;
+
+    public String getNomeEmpresa() {
+        return nomeEmpresa;
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+}
